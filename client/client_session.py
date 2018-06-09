74fb2c0ca3048ea587f74f5583068923288356d3
Lanjiejie <39023511+Lanjiejie@users.noreply.github.com>
2018-06-09 22:23:56 +0800

    Update client_session.py
    加密

--- client/client_session.py
+++ client/client_session.py
@@ -6,6 +6,8 @@ import threading
 import time
 import random
 import queue
+import string
+from secure_transmission import *
 
 HOST = '127.0.0.1'
 PORT = 8080
@@ -20,13 +22,25 @@ class ClientSession:
         self.receiver.start()
         self.lock = threading.Lock()
         self.waiting = None
+        # by lanying 
+        # 随机生成字符串
+        ran_str=''.join(random.sample(string.ascii_letters+string.digits,16))
+        self.AESinstance=AESmessage(ran_str)
+        self.RSAinstance=RSAmessage(newKey=False)
+        self.AESKey_is_init=False
+        # by lanying
 
     def recv(self):
         data = []
         while True:
             time.sleep(0.1)
             res = self.socket.recv(self.MAX_BUFSIZE)
-            res = bytes.decode(res, encoding='utf-8')
+            # by lanying
+            if self.AESKey_is_init:
+                res=self.AESinstance.AESDecrypt(res)
+            else:
+                res = bytes.decode(res, encoding='utf-8')
+            # by lanying
             data.append(res)
             if res.endswith('\r\n'):
                 msg = ''.join(data)
@@ -36,7 +50,18 @@ class ClientSession:
                 if js['type']=='server_response':
                     if js['msg']=='please login':
                         continue
+                    elif js['msg']=='AC_AESKey':    # by lanying
+                        self.AESKey_is_init=True
                     self.waiting = js
+                # by lanying
+                elif js['type']=='init':
+                    if js['msg']=='RSApubKey':
+                        self.waiting = js
+                        self.RSAinstance.pubKey=bytes(js['Key'],encoding='utf-8')
+                        self.waiting_response=True
+                        self.send_AESKey(self.AESinstance.key)
+                        continue
+                # by lanying
                 else:
                     #self.msg_func(js)
                     self.queue.put(js)
@@ -53,11 +78,21 @@ class ClientSession:
         self.waiting = None
         seq = int(time.time()*1000)
         request['timestamp'] = seq
-        s = json.dumps(request, ensure_ascii=False)+'\r\n' # json to string
+        s = json.dumps(request, ensure_ascii=False) # json to string
         print('Send: '+s)
-        self.socket.send(s.encode('utf-8'))
+        # by lanying
+        if self.AESKey_is_init:
+            sendmsg=self.AESinstance.AESEncript(s.encode('utf-8'))
+            self.socket.send(sendmsg)
+            self.socket.send('\r\n'.encode('utf-8'))
+        else:
+            self.socket.send((s+'\r\n').encode('utf-8'))
+        # by lanying
         while self.waiting is None:    # wait for response
             time.sleep(0.5)
+            if self.waiting_response:
+                self.waiting_response=False
+                break
         res = self.waiting
         self.lock.release()
         return res
@@ -178,5 +213,17 @@ class ClientSession:
         res = self.send(request)
         status, msg = res['status'], res['msg']
         return status, msg
+    
+    # by lanying
+    # 这里不设返回值，因为在recv里调用send，send里调用wait造成死锁，所以不等返回
+    def send_AESKey(self,key):
+        # 用RSA公钥加密ASE密钥并发送
+        enckey=self.RSAinstance.RSAEncrypt(key)
+        request = {
+            'type': 'init',
+            'msg': 'AESKey',
+            'Key': str(enckey,encoding='utf-8')
+        }
+        self.send(request)
 
     
